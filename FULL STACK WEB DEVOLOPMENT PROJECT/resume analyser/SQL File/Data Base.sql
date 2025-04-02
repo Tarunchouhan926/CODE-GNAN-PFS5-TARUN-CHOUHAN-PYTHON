15a944f8291c20b75530e0989a779ca5a896466e
Tarun chouhan <Tarunchouhan926@gmail.com>
2025-04-02 12:24:34 +0530

    Add files via upload

---
+++ FULL STACK WEB DEVOLOPMENT PROJECT/resume analyser/SQL File/Data Base.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE resume_analyzer;
+
+USE resume_analyzer;
+
+CREATE TABLE candidates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    cgpa DECIMAL(3, 2),
+    education_details TEXT,
+    ats_score DECIMAL(5, 2)
+);
+select*from candidates;
+ALTER TABLE candidates
+DROP COLUMN name,
+DROP COLUMN location;
