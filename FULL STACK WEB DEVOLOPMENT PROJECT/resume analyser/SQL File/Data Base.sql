1a805fc62c9da4dc5ebe212e7247b60fc1a61133
Tarun chouhan <Tarunchouhan926@gmail.com>
2025-05-20 17:24:31 +0530

    Update Data Base.sql

--- FULL STACK WEB DEVOLOPMENT PROJECT/resume analyser/SQL File/Data Base.sql
+++ FULL STACK WEB DEVOLOPMENT PROJECT/resume analyser/SQL File/Data Base.sql
@@ -3,9 +3,12 @@ CREATE DATABASE resume_analyzer;
 USE resume_analyzer;
 
 CREATE TABLE candidates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
     cgpa DECIMAL(3, 2),
     education_details TEXT,
     ats_score DECIMAL(5, 2)
 );
 select*from candidates;
+
